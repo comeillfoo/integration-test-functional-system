26b28dff301c2bd931bc48e68a7ac523ac09bb45
Come1LLF00 <rayveleaveme34@inbox.ru>
2022-03-07 13:58:02 +0300

    feat: properly integration test; fix: deleted unappropriate tests

--- src/test/java/inc/mimik/functions/SeriedTrigonometricFunctionsTester.java
+++ src/test/java/inc/mimik/functions/SeriedTrigonometricFunctionsTester.java
@@ -19,9 +19,9 @@ public strictfp class SeriedTrigonometricFunctionsTester {
 
   private static SeriedCosine cos = mock( SeriedCosine.class );
 
-  private static SeriedSine sin = null;
-  private static SeriedSecant sec = null;
-  private static SeriedCotangent cot = null;
+  private static SeriedSine stubSin = null;
+  private static SeriedSecant stubSec = null;
+  private static SeriedCotangent stubCot = null;
 
   @BeforeAll
   public static void setup( ) throws IOException, CsvException {
@@ -35,30 +35,76 @@ public strictfp class SeriedTrigonometricFunctionsTester {
     } catch ( IOException | CsvException exp ) {
       throw exp;
     }
-    sin = new SeriedSine( cos );
-    sec = new SeriedSecant( cos );
-    cot = new SeriedCotangent( cos, sin );
+    stubSin = new SeriedSine( cos );
+    stubSec = new SeriedSecant( cos );
+    stubCot = new SeriedCotangent( cos, stubSin );
   }
 
   @ParameterizedTest
   @CsvFileSource( files={ "sine_table.csv" } )
-  public void testSineUsingCSVFile( double x, double expectedY ) {
+  public void testSineOnCosineStubUsingCSVFile( double x, double expectedY ) {
     final double sinDelta = 0.002;
-    assertEquals( expectedY, sin.apply( x ), sinDelta );
+    assertEquals( expectedY, stubSin.apply( x ), sinDelta );
+  }
+
+  @ParameterizedTest
+  @CsvFileSource( files={ "sine_table.csv" } )
+  public void testSineOnRealCosineModuleUsingCSVFile( double x, double expectedY ) {
+    final double sinDelta = 0.08200494902353769;
+    final SeriedCosine realCos = new SeriedCosine( 20 );
+    final SeriedSine realSin = new SeriedSine( realCos );
+    assertEquals( expectedY, realSin.apply( x ), sinDelta );
   }
 
   @ParameterizedTest
   @CsvFileSource( files={ "secant_table.csv" } )
-  public void testSecantUsingCSVFile( double x, double expectedY ) {
+  public void testSecantOnCosineStubUsingCSVFile( double x, double expectedY ) {
     final double secDelta = 0.0049103606726;
-    assertEquals( expectedY, sec.apply( x ), secDelta );
+    assertEquals( expectedY, stubSec.apply( x ), secDelta );
+  }
+
+  @ParameterizedTest
+  @CsvFileSource( files={ "secant_table.csv" } )
+  public void testSecantOnRealCosineModuleUsingCSVFile( double x, double expectedY ) {
+    final double secDelta = 0.0049104448045;
+    final SeriedCosine realCos = new SeriedCosine( 20 );
+    final SeriedSecant realSec = new SeriedSecant( realCos );
+    assertEquals( expectedY, realSec.apply( x ), secDelta );
   }
 
   @ParameterizedTest
   @CsvFileSource( files={ "cotangent_table.csv" } )
-  public void testCotangentUsingCSVFile( double x, double expectedY ) {
+  public void testCotangentOnSineAndCosineStubsUsingCSVFile( double x, double expectedY ) {
     final double cotDelta = 0.4;
-    assertEquals( expectedY, cot.apply( x ), cotDelta );
+    assertEquals( expectedY, stubCot.apply( x ), cotDelta );
+  }
+
+  @ParameterizedTest
+  @CsvFileSource( files={ "cotangent_table.csv" } )
+  public void testCotangentOnSineStubAndRealCosineUsingCSVFile( double x, double expectedY ) {
+    final double cotDelta = 0.4;
+    final SeriedCosine realCos = new SeriedCosine( 20 );
+    final SeriedCotangent almostRealCot = new SeriedCotangent( realCos, stubSin );
+    assertEquals( expectedY, almostRealCot.apply( x ), cotDelta );
+  }
+
+  @ParameterizedTest
+  @CsvFileSource( files={ "cotangent_table.csv" } )
+  public void testCotangentOnRealSineAndCosineStubUsingCSVFile( double x, double expectedY ) {
+    final double cotDelta = 0.4;
+    final SeriedSine realSin = new SeriedSine( new SeriedCosine( 20 ) );
+    final SeriedCotangent almostRealCot = new SeriedCotangent( cos, realSin );
+    assertEquals( expectedY, almostRealCot.apply( x ), cotDelta );
+  }
+
+  @ParameterizedTest
+  @CsvFileSource( files={ "cotangent_table.csv" } )
+  public void testCotangentOnRealCosineAndRealSineUsingCSVFile( double x, double expectedY ) {
+    final double cotDelta = 0.4;
+    final SeriedCosine realCos = new SeriedCosine( 20 );
+    final SeriedSine realSin = new SeriedSine( realCos );
+    final SeriedCotangent realCot = new SeriedCotangent( realCos, realSin );
+    assertEquals( expectedY, realCot.apply( x ), cotDelta );
   }
 
   @ParameterizedTest
@@ -68,6 +114,6 @@ public strictfp class SeriedTrigonometricFunctionsTester {
   } )
   public void testAngleNormalization( double x, double expectedX ) {
     final double delta = 0.001;
-    assertEquals( expectedX, sin.normalize( x ), delta );
+    assertEquals( expectedX, stubSin.normalize( x ), delta );
   }
 }
